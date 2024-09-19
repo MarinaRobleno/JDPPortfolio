3e84a62203b2433dd39ccb96609c6ce206a6fa6a
Marina <marina.robleno@invyo.io>
2024-09-19 18:34:55 +0200

    fix(bug)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -254,7 +254,7 @@ export default function Page() {
                 Contact
               </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Let's get in touch!
+                Let&apos;s get in touch!
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 You can reach me at{" "}
